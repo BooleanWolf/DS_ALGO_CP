78bb9cc1a904bfa133b5c57a9c911d5a82c1792e
BooleanWolf <2010097@me.buet.ac.bd>
2023-12-03 04:15:06 +0600

    Day-1

---
+++ dynamic_programming/frog_1.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+const int max_n = 1e6+1;
+ll dp[max_n];
+ll h[max_n]; 
+int n;
+
+int cost(int pos){
+    if(dp[pos]!=-1) return dp[pos];
+
+
+    if (pos==n) {
+        return dp[pos] = 0; 
+    } 
+
+    if(pos == n-1)
+        return dp[pos]  = abs(h[pos]-h[pos+1]);
+
+    return dp[pos] = min(abs(h[pos]-h[pos+1])+cost(pos+1), abs(h[pos]-h[pos+2])+cost(pos+2));
+}
+
+
+int main()
+{
+    for(int i=0;i<max_n;i++) dp[i] = -1;
+  
+    cin >> n;
+    for(int i=1;i<=n;i++) cin >> h[i];
+
+
+    cout << cost(1) << endl; 
+
+    return 0;
+}
